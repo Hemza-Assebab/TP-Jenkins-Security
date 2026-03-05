c290949fb1df5bbe6a632fa1660c53c085f16ab4
Hemza-Assebab <assebabhamza03@gmail.com>
2026-03-05 22:18:37 +0000

    Added vulnerable dependency and insecure code for testing

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
 def add(a, b):
     return a + b
+
+
+@app.route("/")
+def index():
+    return "Hello Jenkins DevSecOps"
+
+
+# Vulnerable endpoint (command injection)
+@app.route("/run")
+def run_command():
+    cmd = request.args.get("cmd")
+    result = subprocess.getoutput(cmd)  # unsafe execution
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
